c17e2b3b5fb5f1072235ede782216a102dd2b7e9
JuanJoseTJ29 <50315174+JuanJoseTJ29@users.noreply.github.com>
2020-07-14 10:23:03 -0500

    Add files via upload
    inicio del trabajo 1

---
+++ prueba1/parte1.cpp
@@ -0,0 +1,15 @@
+/*Diseñe un algoritmo que permita ingresar en un vector los nombres de las 
+facultades de la UNMSM, luego por cada facultad deberá ingresar los nombres de las
+escuelas profesionales. En base a esta información realizar lo siguiente:
+
+a) Leer los datos.
+b) Mostrar los datos leídos.
+c) Indicar la facultad que tiene el mayor numero de escuelas.
+d) Mostrar la información calculada en el punto C
+*/
+
+#include<iostream>
+#include<string.h>
+using namespace std;
+#define N 100
+
