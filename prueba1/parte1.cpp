b1d4426b63280be1916ffb4151725c5a8877df2a
JuanJoseTJ29 <50315174+JuanJoseTJ29@users.noreply.github.com>
2020-07-14 10:34:56 -0500

    Add files via upload
    se agrego el main

--- prueba1/parte1.cpp
+++ prueba1/parte1.cpp
@@ -13,3 +13,22 @@ d) Mostrar la información calculada en el punto C
 using namespace std;
 #define N 100
 
+
+
+int main()
+{
+	int n,m[N];
+	char esc[N][N][N],nom[N][N];
+	do{
+		cout<<"Ingrese cantidad de facultades de la UNMSM: ";
+		cin>>n;
+	}while(n<0);
+	leer(n,m,esc,nom);
+	mostrar(n,m,esc,nom);
+	cout<<"------------------------------------------------\n";
+	mayorEscuelas(n,m,esc,nom);
+	cout<<"------------------------------------------------\n";
+	mostrarEscuelas(n,m,esc,nom);
+	
+}
+
